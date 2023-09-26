d1f437f1f34723db161a576267021e6aa4734153
kibarmustofa <kibarmustofa115@gmail.com>
2023-09-26 15:40:56 +0700

    P5 pemilihan percobaaan 1

---
+++ P5/PemilihanPercobaan115.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class PemilihanPercobaan115 {
+
+    public static void main(String[] args) {
+        Scanner input00 = new Scanner(System.in);
+
+        System.out.print("Masukan angka: ");
+        int angka = input00.nextInt();
+
+        String AngkaBilanganBulat = (angka % 2 == 0) ? "bilangan genap" : "bilangan ganjil";
+        System.out.println("Angka " + angka + " adalah " + AngkaBilanganBulat );
+    }
+}
+    
