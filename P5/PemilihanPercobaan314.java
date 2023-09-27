b6d7d9dafcc5d86f03b7f40e1d7722772f601185
kibarmustofa <kibarmustofa115@gmail.com>
2023-09-27 08:04:10 +0700

    hasil modifikasi

---
+++ P5/PemilihanPercobaan314.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class PemilihanPercobaan314 {
+
+    public static void main(String[] args) {
+    double angka1, angka2, hasil;
+    char operator;
+    
+    Scanner input14 = new Scanner(System.in);
+    System.out.print("Masukan angka pertama: ");
+    angka1 = input14.nextDouble();
+    System.out.print("Masukan angka kedua:");
+    angka2 = input14.nextDouble();
+    System.out.print("Masukan operator (+ - * /):");
+    operator = input14.next().charAt(0);
+
+ switch (operator) {
+    case '+':
+        hasil = angka1 + angka2;
+        System.out.println(angka1 + "+" + angka2 + "=" + hasil); 
+        break;
+    case '-':
+        hasil = angka1 + angka2;
+        System.out.println(angka1 + "-" + angka2 + "=" + hasil); 
+        break;
+    case '*':
+        hasil = angka1 + angka2;
+        System.out.println(angka1 + "*" + angka2 + "=" + hasil); 
+        break;
+    case '/':
+        hasil = angka1 + angka2;
+        System.out.println(angka1 + "/" + angka2 + "=" + hasil); 
+        break;
+        }
+    }
+}
