e3e50e29a755cb79c6994af98cd03dfa6ed7ae7f
kibarmustofa <kibarmustofa115@gmail.com>
2023-09-27 08:05:54 +0700

    perubahan nama

--- P5/PemilihanPercobaan114.java
+++ P5/PemilihanPercobaan114.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class PemilihanPercobaan114 {
 
     public static void main(String[] args) {
-        Scanner input00 = new Scanner(System.in);
+        Scanner input14 = new Scanner(System.in);
 
         System.out.print("Masukan angka: ");
-        int angka = input00.nextInt();
+        int angka = input14.nextInt();
 
         String AngkaBilanganBulat = (angka % 2 == 0) ? "bilangan genap" : "bilangan ganjil";
         System.out.println("Angka " + angka + " adalah " + AngkaBilanganBulat );
