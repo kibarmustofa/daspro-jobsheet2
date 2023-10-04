8bc49c5169e1290ee70b8c146a815e4707037f0a
kibarmustofa <kibarmustofa115@gmail.com>
2023-10-04 08:14:51 +0700

    modifikasi percobaan 1 tugass ke 4

--- P6/Pemilihan2Percobaan114.java
+++ P6/Pemilihan2Percobaan114.java
@@ -9,11 +9,17 @@ public class Pemilihan2Percobaan114 {
     tahun = input14.nextInt();
 
     if ((tahun %4) == 0 ){
-        if((tahun % 100) !=0)  {
+        if((tahun % 100) ==0)  {
+            if((tahun % 400)==0) {
         System.out.println("Tahun kabisat");
-    } else 
+    } else {
     System.out.println("Bukan Tahun Kabisat");
-    
     }
-  }
+    } else {
+    System.out.println("Tahun Kabisat");
+    }
+    } else {
+    System.out.println("Bukan Tahun Kabisat");
+    }
+}
 }
