1fceb0b0481d000827b24d8c9771815dd096c3aa
kibarmustofa <kibarmustofa115@gmail.com>
2023-10-16 07:03:32 +0700

    for kelipatan

---
+++ P7/forKelipatan14.java
@@ -0,0 +1,23 @@
+import java.util.Scanner ;
+public class forKelipatan14 {
+    public static void main(String[] args) { 
+        Scanner scan  = new Scanner(System.in);
+
+        int kelipatan,total,counter ;
+        total = 0 ;
+        counter = 0 ;
+
+        System.out.print("masukkan bilangan kelipatan (1-9): ");
+        kelipatan = scan.nextInt();
+
+        for (int i = 1; i <= 50; i++ ) {  //perulangan for
+            if (i % kelipatan == 0){
+                total += i;
+                counter++;
+            }   
+        }    
+        System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n",kelipatan, counter );
+        System.out.printf("Total bilangan keipatan %d dari 1 sampai 50 persen adalah %d\n", kelipatan, total );
+         
+    }
+}
