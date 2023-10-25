ed5c6f015e65c83aff40497bd0ab15d947d06e63
kibarmustofa <kibarmustofa115@gmail.com>
2023-10-25 07:59:08 +0700

    ganti nama

---
+++ p9/ArrayBilangan14.java
@@ -0,0 +1,16 @@
+public class ArrayBilangan14 {
+    public static void main(String []args ) {
+        int[] bil = new int[4]; // array 1 
+        
+        bil[0] = 1;
+        bil[1] = 13;
+        bil[2] = -7;
+        bil[3] = 17;
+
+        System.out.println(bil[0]);
+        System.out.println(bil[1]);
+        System.out.println(bil[2]);
+        System.out.println(bil[3]);
+
+    }
+}
