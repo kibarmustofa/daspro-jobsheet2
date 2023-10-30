2104a21369e2a960af4f92b9e9c427f6905f3c54
kibarmustofa <kibarmustofa115@gmail.com>
2023-10-30 13:44:17 +0700

    modiifikasi linear search14

--- p9/LinearSearch14.java
+++ p9/LinearSearch14.java
@@ -1,19 +1,32 @@
+import java.util.Scanner;
+ 
 public class LinearSearch14 {
  
     public static void main(String[] args) {
-        
-        int [] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63}; //inisialisasi
-        int key = 20;
+    Scanner sc = new Scanner(System.in);
+        int jmlArray, key;
         int hasil = 0;
  
-        for (int i = 0; i < arrayInt.length; i++) {
-            if (arrayInt [i] == key) { //kondisi
-                hasil = i;
-                break;
+        System.out.print("Masukkan jumlah elemen array : ");
+        jmlArray = sc.nextInt();
+        int [] arrayInt = new int [jmlArray];
+ 
+            for (int i = 0; i < arrayInt.length; i++) {
+            System.out.print("Masukkan elemen array ke-"+i+" : ");
+            arrayInt [i] = sc.nextInt ();
             }
-        }
+        
+            System.out.print("Masukkan key yang ingin dicari : ");
+            key = sc.nextInt();
+ 
+            for (int i = 0; i < arrayInt.length; i++) {
+                if (arrayInt [i] == key) {
+                    hasil = i;
+                    break;
+                }
+            }
  
         System.out.println("Key ada dalam array pada posisi indeks ke-" +hasil);
-    } //tampilan
+    }
     
 }
