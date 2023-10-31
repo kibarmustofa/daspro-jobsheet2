a12e3c839125c67185917810b69eaf652a95c95e
kibarmustofa <kibarmustofa115@gmail.com>
2023-11-01 02:24:47 +0700

    modifikasi menggunakan for

--- P10/Bioskop14.java
+++ P10/Bioskop14.java
@@ -2,6 +2,11 @@ public class Bioskop14 {
     public static void main (String[] args) {
         String[][] penonton = new String[4][2];
 
+        System.out.println(penonton.length);
+
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Panjang baris ke-" + (i+1) + ":" + penonton[i].length);
+        }
         penonton[0][0] = "Amin";  //deklarasi ARRAY 
         penonton[0][1] = "Bena";
         penonton[1][0] = "candra";
@@ -9,7 +14,8 @@ public class Bioskop14 {
         penonton[2][0] = "Eka";
         penonton[2][1] = "Farhan";
         penonton[3][0] = "Gisel";
-
+        penonton[3][1] = "Hana";
+   
         System.out.printf("%s \t %s \n", penonton[0][0],penonton[0][1]);
         System.out.printf("%s \t %s \n", penonton[1][0],penonton[1][1]);
         System.out.printf("%s \t %s \n", penonton[2][0],penonton[2][1]);
