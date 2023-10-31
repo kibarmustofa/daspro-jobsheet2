f46742405c544b426a5c00dba8a80d67f3cb2cc6
kibarmustofa <kibarmustofa115@gmail.com>
2023-11-01 02:54:24 +0700

    menggunakan foreach loop

--- P10/Bioskop14.java
+++ P10/Bioskop14.java
@@ -1,13 +1,8 @@
 public class Bioskop14 {
     public static void main (String[] args) {
-        String[][] penonton = new String[4][2]; // deklarasi ARRAY
+        String[][] penonton = new String[4][2]; //deklarasi ARRAY 
 
-        System.out.println(penonton.length);
-
-        for (int i = 0; i < penonton.length; i++) {  // for loop
-            System.out.println("Panjang baris ke-" + (i+1) + ":" + penonton[i].length);
-        }
-        penonton[0][0] = "Amin";  // input array
+        penonton[0][0] = "Amin";  //input ARRAY 
         penonton[0][1] = "Bena";
         penonton[1][0] = "candra";
         penonton[1][1] = "Dela";
@@ -15,7 +10,13 @@ public class Bioskop14 {
         penonton[2][1] = "Farhan";
         penonton[3][0] = "Gisel";
         penonton[3][1] = "Hana";
-   
+        
+        System.out.println(penonton.length);
+        
+
+        for (String[] barisPenonton : penonton) { // foreach loop 
+            System.out.println("panjang baris: " + barisPenonton.length);
+        }
         System.out.printf("%s \t %s \n", penonton[0][0],penonton[0][1]);
         System.out.printf("%s \t %s \n", penonton[1][0],penonton[1][1]);
         System.out.printf("%s \t %s \n", penonton[2][0],penonton[2][1]);
