f2398b1bdd6dc503b68c02ff21a1ea7c42150e43
kibarmustofa <kibarmustofa115@gmail.com>
2023-11-01 08:13:29 +0700

    penambahan keterangan

--- P10/BioskopWithScanner14.java
+++ P10/BioskopWithScanner14.java
@@ -3,12 +3,12 @@ public class BioskopWithScanner14 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); 
 
-        int baris, kolom;
+        int baris, kolom; // inisialisasi variabel 
         String nama, next;
 
-        String[][] penonton = new String[4][2];
+        String[][] penonton = new String[4][2]; // inisialisasi array
         
-        while (true) {
+        while (true) { //perulangan 
             System.out.print("masukan nama: ");
             nama = sc.nextLine();
             System.out.print("masukan baris: ");
@@ -18,9 +18,9 @@ public class BioskopWithScanner14 {
             sc.nextLine();
             penonton [baris-1][kolom-1] = nama;
 
-            System.out.print("Input penonton lainnya? (y/n): ");
+            System.out.print("Input penonton lainnya? (y/n): "); // tampilan 
             next = sc.nextLine ();
-            if (next.equalsIgnoreCase("n")){
+            if (next.equalsIgnoreCase("n")){ //pemilihan 
                 break;
             }
         }
