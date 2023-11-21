0adbad00cdada06b902d05277527e016f3da915b
kibarmustofa <kibarmustofa115@gmail.com>
2023-11-21 14:08:36 +0700

    percobaan 1

---
+++ P12/terimakasih_14.java
@@ -0,0 +1,9 @@
+package P12;
+public class terimakasih_14 {
+    public static void UcapanTerimaKasih() {
+        System.out.println("Thank you for being the best teacher in the world.\n"+"You inspired in me a love for learning and mede me feel like i could ask you anything. ");
+    }
+    public static void main(String[]args){
+        UcapanTerimaKasih();
+    }
+}
